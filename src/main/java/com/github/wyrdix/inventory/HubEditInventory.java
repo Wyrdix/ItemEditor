9755ee4956badb3176224c367a8b6821858ad4c8
Wyrdix <nathancoquerel@gmail.com>
2020-11-29 11:46:32 +0100

    Add a task to remove name_writers after one minute

--- src/main/java/com/github/wyrdix/inventory/HubEditInventory.java
+++ src/main/java/com/github/wyrdix/inventory/HubEditInventory.java
@@ -7,9 +7,6 @@ import fr.minuskube.inv.content.InventoryContents;
 import fr.minuskube.inv.content.InventoryProvider;
 import fr.minuskube.inv.content.SlotPos;
 import net.md_5.bungee.api.ChatMessageType;
-import net.md_5.bungee.api.chat.BaseComponent;
-import net.md_5.bungee.api.chat.ClickEvent;
-import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -17,18 +14,17 @@ import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
-import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerItemHeldEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.scheduler.BukkitRunnable;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
-public class HubEditInventory implements InventoryProvider, Listener {
+public class HubEditInventory extends BukkitRunnable implements InventoryProvider, Listener {
 
   private static final ItemStack DUMMY = new ItemStack(Material.STAINED_GLASS_PANE, 1, (short) 7);
 
@@ -37,7 +33,6 @@ public class HubEditInventory implements InventoryProvider, Listener {
   private static final ItemStack NAME = new ItemStack(Material.SIGN);
 
   private static final ItemStack LORE = new ItemStack(Material.BOOK_AND_QUILL);
-
 
   private static final SmartInventory INVENTORY;
 
@@ -67,17 +62,24 @@ public class HubEditInventory implements InventoryProvider, Listener {
       LORE.setItemMeta(meta);
     }
 
-    INVENTORY = SmartInventory.builder()
-            .id("ie:hubEdit")
-            .provider(new HubEditInventory())
-            .title("§cEdit")
-            .manager(ItemEditorPlugin.getInstance().getInvManager()).build();
+    INVENTORY =
+            SmartInventory.builder()
+                    .id("ie:hubEdit")
+                    .provider(new HubEditInventory())
+                    .title("§cEdit")
+                    .manager(ItemEditorPlugin.getInstance().getInvManager())
+                    .build();
   }
 
-  private final List<Player> name_writers = new ArrayList<>();
+  private final Map<Player, Long> name_writers = new HashMap<>();
+  private final TextComponent name_writing_comp =
+          new TextComponent("§7Write in the chat the name (or && to remove it)");
+  private final TextComponent name_writing_stop_comp =
+          new TextComponent("§cYou don't have written something, action cancelled");
 
   private HubEditInventory() {
     Bukkit.getPluginManager().registerEvents(this, ItemEditorPlugin.getInstance());
+    runTaskTimerAsynchronously(ItemEditorPlugin.getInstance(), 0, 20);
   }
 
   public static void open(Player player) {
@@ -86,30 +88,41 @@ public class HubEditInventory implements InventoryProvider, Listener {
 
   public void init(Player player, InventoryContents contents) {
 
-    contents.fillRect(0,0, INVENTORY.getRows()-1, INVENTORY.getColumns()-1, ClickableItem.empty(DUMMY));
+    contents.fillRect(
+            0, 0, INVENTORY.getRows() - 1, INVENTORY.getColumns() - 1, ClickableItem.empty(DUMMY));
 
-    contents.set(SlotPos.of(3,2), ClickableItem.empty(ENCHANT));
-    contents.set(SlotPos.of(2, 6), ClickableItem.of(NAME,
-            (s)-> {
-      name_writers.add(player);
-      INVENTORY.close(player);
-      player.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent("§7Write in the chat the name (or && to remove it)"));
-            }));
+    PlayerInventory inv = player.getInventory();
+    contents.setProperty("item", inv.getItemInMainHand());
+
+    contents.set(SlotPos.of(3, 2), ClickableItem.empty(ENCHANT));
+    contents.set(
+            SlotPos.of(2, 6),
+            ClickableItem.of(
+                    NAME,
+                    (s) -> {
+                      name_writers.put(player, System.currentTimeMillis());
+                      INVENTORY.close(player);
+                      player.spigot().sendMessage(ChatMessageType.ACTION_BAR, name_writing_comp);
+                    }));
     contents.set(SlotPos.of(3, 7), ClickableItem.empty(LORE));
   }
 
-  public void update(Player player, InventoryContents contents) {}
+  public void update(Player player, InventoryContents contents) {
+    contents.set(SlotPos.of(0, 4), ClickableItem.empty(contents.property("item")));
+  }
 
   @EventHandler
-  public void slotChange(PlayerItemHeldEvent event){
-    if(!name_writers.contains(event.getPlayer())) return;
+  public void slotChange(PlayerItemHeldEvent event) {
+    if (!name_writers.containsKey(event.getPlayer())) return;
     name_writers.remove(event.getPlayer());
-    event.getPlayer().sendMessage("§cThe edition of name was cancelled because you have change your held item");
+    event
+            .getPlayer()
+            .sendMessage("§cThe edition of name was cancelled because you have change your held item");
   }
 
   @EventHandler(ignoreCancelled = true)
-  public void onMessage(AsyncPlayerChatEvent event){
-    if(!name_writers.contains(event.getPlayer())) return;
+  public void onMessage(AsyncPlayerChatEvent event) {
+    if (!name_writers.containsKey(event.getPlayer())) return;
     event.setCancelled(true);
     name_writers.remove(event.getPlayer());
     String custom = ChatColor.translateAlternateColorCodes('&', event.getMessage());
@@ -117,10 +130,27 @@ public class HubEditInventory implements InventoryProvider, Listener {
     ItemStack mainHand = inv.getItemInMainHand();
     ItemMeta meta = mainHand.getItemMeta();
     assert meta != null;
-    if(custom.equals("&&")) meta.setDisplayName(null);
+    if (custom.equals("&&")) meta.setDisplayName(null);
     else meta.setDisplayName(custom);
     mainHand.setItemMeta(meta);
     event.getPlayer().sendMessage("§6Name changed");
     INVENTORY.open(event.getPlayer());
+  }
+
+  @Override
+  public void run() {
+    final long millis = System.currentTimeMillis();
+    name_writers
+            .entrySet()
+            .removeIf(
+                    (e) -> {
+                      if (e.getValue() + 60000 <= millis) {
+                        e.getKey().spigot().sendMessage(ChatMessageType.ACTION_BAR, name_writing_stop_comp);
+                        return true;
+                      } else return false;
+                    });
+    for (Player player : name_writers.keySet()) {
+      player.spigot().sendMessage(ChatMessageType.ACTION_BAR, name_writing_comp);
+    }
   }
 }
